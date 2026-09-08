0f3d85d28e389cfcddbace39f7004789cd67362e
Christine Ngo <cnngo@wpi.edu>
2026-09-08 02:20:19 -0400

    Add formats option to RepoSense command

--- run.sh
+++ run.sh
@@ -16,4 +16,4 @@
 ## Examples of other valid options; For more, please view the user guide
 ### java -jar RepoSense.jar --repos https://github.com/reposense/RepoSense.git
 
-java -jar RepoSense.jar --since 06/01/2026
+java -jar RepoSense.jar --since 06/01/2026 --formats fx
